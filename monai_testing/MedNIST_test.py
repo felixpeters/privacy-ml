014b73ea6d10472cd4c30d0ec733e4b3add91b8e
AdrianGlauben <adrianphilip.glauben@stud.tu-darmstadt.de>
2021-05-20 13:35:11 +0200

    Monai test commit

---
+++ monai_testing/MedNIST_test.py
@@ -0,0 +1,254 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import tempfile
+import matplotlib.pyplot as plt
+import PIL
+import torch
+import numpy as np
+from sklearn.metrics import classification_report
+from tqdm import tqdm
+
+from monai.apps import download_and_extract
+from monai.config import print_config
+from monai.metrics import compute_roc_auc
+from monai.networks.nets import DenseNet121
+from monai.transforms import (
+    Activations,
+    AddChannel,
+    AsDiscrete,
+    Compose,
+    LoadImage,
+    RandFlip,
+    RandRotate,
+    RandZoom,
+    ScaleIntensity,
+    ToTensor,
+)
+from monai.utils import set_determinism
+
+if __name__ == '__main__':
+    # Setting up data directory
+    root_dir = './Data'
+
+    # Getting data
+    DOWNLOAD = False
+
+    resource = "https://www.dropbox.com/s/5wwskxctvcxiuea/MedNIST.tar.gz?dl=1"
+    md5 = "0bc7306e7427e00ad1c5526a6677552d"
+
+    compressed_file = os.path.join(root_dir, "MedNIST.tar.gz")
+    data_dir = os.path.join(root_dir, "MedNIST")
+
+    if DOWNLOAD:
+        download_and_extract(resource, compressed_file, root_dir, md5)
+
+    # Setting deterministic training to ensure reproducibility
+    set_determinism(seed=42)
+
+    # Reading image filenames from dataset folders and assigning labels
+    class_names = sorted(x for x in os.listdir(data_dir)
+                         if os.path.isdir(os.path.join(data_dir, x)))
+    num_class = len(class_names)
+    image_files = [
+        [
+            os.path.join(data_dir, class_names[i], x)
+            for x in os.listdir(os.path.join(data_dir, class_names[i]))
+        ]
+        for i in range(num_class)
+    ]
+    num_each = [len(image_files[i]) for i in range(num_class)]
+    image_files_list = []
+    image_class = []
+    for i in range(num_class):
+        image_files_list.extend(image_files[i])
+        image_class.extend([i] * num_each[i])
+    num_total = len(image_class)
+    image_width, image_height = PIL.Image.open(image_files_list[0]).size
+
+    # Train, Validation and Test split
+    val_frac = 0.1
+    test_frac = 0.1
+    length = len(image_files_list)
+    indices = np.arange(length)
+    np.random.shuffle(indices)
+
+    test_split = int(test_frac * length)
+    val_split = int(val_frac * length) + test_split
+    test_indices = indices[:test_split]
+    val_indices = indices[test_split:val_split]
+    train_indices = indices[val_split:]
+
+    train_x = [image_files_list[i] for i in train_indices]
+    train_y = [image_class[i] for i in train_indices]
+    val_x = [image_files_list[i] for i in val_indices]
+    val_y = [image_class[i] for i in val_indices]
+    test_x = [image_files_list[i] for i in test_indices]
+    test_y = [image_class[i] for i in test_indices]
+
+    # MONAI transforms, Dataset and Dataloader for preprocessing
+    train_transforms = Compose(
+        [
+            LoadImage(image_only=True),
+            AddChannel(),
+            ScaleIntensity(),
+            RandRotate(range_x=np.pi / 12, prob=0.5, keep_size=True),
+            RandFlip(spatial_axis=0, prob=0.5),
+            RandZoom(min_zoom=0.9, max_zoom=1.1, prob=0.5),
+            ToTensor(),
+        ]
+    )
+
+    val_transforms = Compose(
+        [LoadImage(image_only=True), AddChannel(), ScaleIntensity(), ToTensor()])
+
+    act = Activations(softmax=True)
+    to_onehot = AsDiscrete(to_onehot=True, n_classes=num_class)
+
+
+    class MedNISTDataset(torch.utils.data.Dataset):
+        def __init__(self, image_files, labels, transforms):
+            self.image_files = image_files
+            self.labels = labels
+            self.transforms = transforms
+
+        def __len__(self):
+            return len(self.image_files)
+
+        def __getitem__(self, index):
+            return self.transforms(self.image_files[index]), self.labels[index]
+
+    BATCH_SIZE = 300
+
+    train_ds = MedNISTDataset(train_x, train_y, train_transforms)
+    train_loader = torch.utils.data.DataLoader(
+        train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+
+    val_ds = MedNISTDataset(val_x, val_y, val_transforms)
+    val_loader = torch.utils.data.DataLoader(
+        val_ds, batch_size=BATCH_SIZE, num_workers=0)
+
+    test_ds = MedNISTDataset(test_x, test_y, val_transforms)
+    test_loader = torch.utils.data.DataLoader(
+        test_ds, batch_size=BATCH_SIZE, num_workers=0)
+
+    # Defining Network and Optimizer
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+
+    model = DenseNet121(spatial_dims=2, in_channels=1,
+                        out_channels=num_class).to(device)
+    loss_function = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), 1e-5)
+    max_epochs = 4
+    val_interval = 1
+
+    # Model training
+    best_metric = -1
+    best_metric_epoch = -1
+    epoch_loss_values = []
+    metric_values = []
+
+    for epoch in range(max_epochs):
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{max_epochs}")
+        model.train()
+        epoch_loss = 0
+        step = 0
+        for batch_data in tqdm(train_loader):
+            step += 1
+            inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            epoch_len = len(train_ds) // train_loader.batch_size
+        epoch_loss /= step
+        epoch_loss_values.append(epoch_loss)
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if (epoch + 1) % val_interval == 0:
+            model.eval()
+            with torch.no_grad():
+                y_pred = torch.tensor([], dtype=torch.float32, device=device)
+                y = torch.tensor([], dtype=torch.long, device=device)
+                for val_data in val_loader:
+                    val_images, val_labels = (
+                        val_data[0].to(device),
+                        val_data[1].to(device),
+                    )
+                    y_pred = torch.cat([y_pred, model(val_images)], dim=0)
+                    y = torch.cat([y, val_labels], dim=0)
+                y_onehot = to_onehot(y)
+                y_pred_act = act(y_pred)
+                auc_metric = compute_roc_auc(y_pred_act, y_onehot)
+                del y_pred_act, y_onehot
+                metric_values.append(auc_metric)
+                acc_value = torch.eq(y_pred.argmax(dim=1), y)
+                acc_metric = acc_value.sum().item() / len(acc_value)
+                if auc_metric > best_metric:
+                    best_metric = auc_metric
+                    best_metric_epoch = epoch + 1
+                    torch.save(model.state_dict(), os.path.join(
+                        root_dir, "best_metric_model.pth"))
+                    print("saved new best metric model")
+                print(
+                    f"current epoch: {epoch + 1} current AUC: {auc_metric:.4f}"
+                    f" current accuracy: {acc_metric:.4f}"
+                    f" best AUC: {best_metric:.4f}"
+                    f" at epoch: {best_metric_epoch}"
+                )
+
+    print(
+        f"train completed, best_metric: {best_metric:.4f} "
+        f"at epoch: {best_metric_epoch}")
+
+    # Plotting metrics
+    plt.figure("train", (12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Epoch Average Loss")
+    x = [i + 1 for i in range(len(epoch_loss_values))]
+    y = epoch_loss_values
+    plt.xlabel("epoch")
+    plt.plot(x, y)
+    plt.subplot(1, 2, 2)
+    plt.title("Val AUC")
+    x = [val_interval * (i + 1) for i in range(len(metric_values))]
+    y = metric_values
+    plt.xlabel("epoch")
+    plt.plot(x, y)
+    plt.show()
+
+    # Evaluating model
+    model.load_state_dict(torch.load(
+        os.path.join(root_dir, "best_metric_model.pth")))
+    model.eval()
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for test_data in test_loader:
+            test_images, test_labels = (
+                test_data[0].to(device),
+                test_data[1].to(device),
+            )
+            pred = model(test_images).argmax(dim=1)
+            for i in range(len(pred)):
+                y_true.append(test_labels[i].item())
+                y_pred.append(pred[i].item())
+
+    print(classification_report(
+        y_true, y_pred, target_names=class_names, digits=4))
